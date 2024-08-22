5a55e36a63074a892bd67fefcec3fe9912f98627
akshaysatoskar <119950526+akshaysatoskar@users.noreply.github.com>
2024-08-22 19:21:38 +0530

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Akshay Satoskar on 22/08/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
